2d2baa852602f92127be3157263b8681f27d6fc4
macdrien <adrien.bouyssou@gmail.com>
2023-11-27 14:33:36 +0100

    ADD register endpoint

--- src/main/java/fr/sidranie/endpoints/UserController.java
+++ src/main/java/fr/sidranie/endpoints/UserController.java
@@ -3,16 +3,21 @@ package fr.sidranie.endpoints;
 import java.time.Instant;
 import java.util.Date;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.resteasy.reactive.server.jaxrs.ResponseBuilderImpl;
 
 import fr.sidranie.auth.Credential;
 import fr.sidranie.dao.UserDao;
+import fr.sidranie.endpoints.dto.CreateUser;
 import fr.sidranie.entities.Session;
 import fr.sidranie.entities.User;
 import fr.sidranie.services.SessionService;
+import io.quarkus.logging.Log;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.POST;
@@ -22,6 +27,7 @@ import jakarta.ws.rs.core.NewCookie;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.NewCookie.SameSite;
 import jakarta.ws.rs.core.Response.ResponseBuilder;
+import jakarta.ws.rs.core.Response.Status;
 
 @Path("/users")
 public class UserController {
@@ -62,5 +68,46 @@ public class UserController {
 
         ResponseBuilder builder = Response.ok(userOptional.get()).cookie(sessionCookie);
         return builder.build();
+    }
+
+    @POST
+    @Path("/register")
+    @Transactional
+    public Response register(CreateUser newUser) {
+        List<String> errors = new ArrayList<>();
+        if (newUser.username == null || newUser.username.length() < 3) {
+            errors.add("Le username est obligatoire et doit faire plus de 3 caractères.");
+        }
+        if (newUser.email == null || newUser.email.isBlank()) {
+            errors.add("L'adresse mail est obligatoire.");
+        }
+        if (newUser.password == null || newUser.password.length() <= 3) {
+            errors.add("Le mot de passe est obligatoire et doit faire 3 caractères ou plus.");
+        }
+        if (errors.size() != 0) {
+            return new ResponseBuilderImpl()
+                .entity(errors, null)
+                .status(Status.BAD_REQUEST)
+                .build();
+        }
+
+        User user = new User();
+        user.username = newUser.username;
+        user.password = newUser.password;
+        user.email = newUser.email;
+        userDao.persist(user);
+
+        Session session = sessionService.initializeSessionForUser(user);
+        NewCookie sessionCookie = new NewCookie.Builder("session")
+                .sameSite(SameSite.STRICT)
+                .value(session.id)
+                .expiry(new Date(session.creation.plusMillis(expiration).toEpochMilli()))
+                .build();
+
+        return new ResponseBuilderImpl()
+            .entity(user, null)
+            .cookie(sessionCookie)
+            .status(Status.CREATED)
+            .build();
     }
 }
---
+++ src/main/java/fr/sidranie/endpoints/dto/CreateUser.java
@@ -0,0 +1,7 @@
+package fr.sidranie.endpoints.dto;
+
+public class CreateUser {
+    public String username;
+    public String password;
+    public String email;
+}
