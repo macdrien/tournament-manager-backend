cf3f802c04228569c4b3e961ff606dd663ba8d25
macdrien <adrien.bouyssou@gmail.com>
2023-11-30 13:41:45 +0100

    REF Set multi sessions for one user

--- src/main/java/fr/sidranie/entities/Session.java
+++ src/main/java/fr/sidranie/entities/Session.java
@@ -6,7 +6,7 @@ import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Session extends PanacheEntityBase {
@@ -14,8 +14,8 @@ public class Session extends PanacheEntityBase {
     @Id
     public String id;
 
-    @OneToOne(targetEntity = User.class)
-    @JoinColumn(name = "userId")
+    @ManyToOne(optional=false) 
+    @JoinColumn(name="userId", nullable=false, updatable=false)
     public User user;
 
     public Instant creation;
