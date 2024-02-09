0ec3740a30482bb02c7e7d4a4dbbc368526653d7
macdrien <adrien.bouyssou@gmail.com>
2024-02-09 22:20:49 +0100

    ADD Complete getters for role endpoint

--- src/main/java/fr/sidranie/dao/RoleDao.java
+++ src/main/java/fr/sidranie/dao/RoleDao.java
@@ -2,9 +2,14 @@ package fr.sidranie.dao;
 
 import fr.sidranie.entities.Role;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.panache.common.Parameters;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class RoleDao implements PanacheRepository<Role> {
-  // Empty body
+
+  public Role findByName(String roleName) {
+    Parameters parameters = Parameters.with("name", roleName);
+    return find("name = :name", parameters).firstResult();
+  }
 }
--- src/main/java/fr/sidranie/endpoints/RoleController.java
+++ src/main/java/fr/sidranie/endpoints/RoleController.java
@@ -2,20 +2,37 @@ package fr.sidranie.endpoints;
 
 import java.util.List;
 
+import fr.sidranie.auth.filters.annotations.Authenticated;
 import fr.sidranie.dao.RoleDao;
 import fr.sidranie.entities.Role;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 
-@Path("/roles")
+@Path("roles")
 public class RoleController {
 
     @Inject
     RoleDao roleDao;
 
     @GET
+    @Authenticated
     public List<Role> listAll() {
         return roleDao.listAll();
+    }
+
+    @GET
+    @Path("{id}")
+    @Authenticated
+    public Role getById(@PathParam("id") Long roleId) {
+        return roleDao.findById(roleId);
+    }
+
+    @GET
+    @Path("{name}")
+    @Authenticated
+    public Role getByName(@PathParam("name") String roleName) {
+        return roleDao.findByName(roleName);
     }
 }
